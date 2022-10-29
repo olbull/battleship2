6d9da9491e98f20bd0ce1c881cd3d5d1f65d3fab
sbenzo99 <stefano.benzoni@uzh.ch>
2022-10-29 13:02:56 +0200

    Mit Fleet Iterator implementiert

--- Battleship/src/Fleet/Fleet.java
+++ Battleship/src/Fleet/Fleet.java
@@ -1,16 +1,82 @@
 package Fleet;
 import java.util.ArrayList;
-
 import Coordinate.ShipCoordinate;
-import Ships.Ship;
+import Ships.*;
+import java.util.*;
+import java.lang.Iterable;
 
-public class Fleet {
-    ArrayList<Ship> ships;
-    boolean alive;
+public class Fleet implements Iterable<Ships.Ship>{
+    public final ArrayList<Ships.Ship> fleet = new ArrayList<>();
+    boolean alive = true;
+    public final int fleetSize = 10;
 
-    public ShipCoordinate placeShips(){
-        return null;
+    public Fleet(){
+        initializeFleet();
     }
 
-    public void checkShips(){}
+    @Override
+    public Iterator<Ship> iterator(){
+        Iterator<Ship> iter = new Iterator(){
+            private int currentIndex = 0;
+            @Override
+            public boolean hasNext(){
+                return currentIndex < fleetSize; // Nr Ships in Fleet
+
+            }
+            @Override
+            public Ship next(){
+                if (hasNext()) return fleet.get(currentIndex++);
+                else throw new UnsupportedOperationException("Not supported");
+            }
+        };
+        return iter;
+    }
+
+
+
+
+    public void placeShips(){
+        /*boolean[][] controlMatrix = new boolean[10][10];
+        for(int i = 0; i < controlMatrix.length; i++){
+            for(int j = 0; j < controlMatrix.length; j++){
+                controlMatrix[i][j] = false;
+            }
+        }*/
+
+        Iterator<Ship> iter = fleet.iterator();
+        while (iter.hasNext()) {
+            Ship s = iter.next();
+            ShipCoordinate temp = s.placeShip();
+        }
+    }
+
+    public void checkShips(){
+        // Iterator<Ship> iterator = fleet.iterator();
+    }
+
+    public void initializeFleet(){
+        fleet.add(new Carrier());
+        fleet.add(new BattleShip());
+        fleet.add(new BattleShip());
+        fleet.add(new Submarine());
+        fleet.add(new Submarine());
+        fleet.add(new Submarine());
+        fleet.add(new PatrolBoat());
+        fleet.add(new PatrolBoat());
+        fleet.add(new PatrolBoat());
+        fleet.add(new PatrolBoat());
+
+        //fleet[0] = new Carrier();
+        //fleet[1] = new BattleShip();
+        //fleet[2] = new BattleShip();
+        //fleet[3] = new Submarine();
+        //fleet[4] = new Submarine();
+        //fleet[5] = new Submarine();
+        //fleet[6] = new PatrolBoat();
+        //fleet[7] = new PatrolBoat();
+        //fleet[8] = new PatrolBoat();
+        //fleet[9] = new PatrolBoat();
+    }
+
 }
+
--- Battleship/src/Ships/BattleShip.java
+++ Battleship/src/Ships/BattleShip.java
@@ -1,8 +1,17 @@
 package Ships;
 
+import Coordinate.ShipCoordinate;
+
 public class BattleShip extends Ship {
-    public BattleShip() {
+    public BattleShip(){
         this.length = 4;
         this.letter = 'B';
     }
+
+    public ShipCoordinate placeShip(){
+        return null;
+    }
+
+
+
 }
